c01c356b9b44911d3418e1e96bb9e6b7a9f8ba71
VanKuang <vanchee@qq.com>
2016-06-15 22:44:28 +0800

    Try regular singleton.

---
+++ java-core/src/main/java/cn/van/kuang/hello/world/java/design/pattern/singleton/Singleton.java
@@ -0,0 +1,19 @@
+package cn.van.kuang.hello.world.java.design.pattern.singleton;
+
+public class Singleton {
+
+    public static Singleton instance() {
+        return Holder.singleton;
+    }
+
+    private static class Holder {
+        private final static Singleton singleton = new Singleton();
+    }
+
+    private Singleton() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Singleton.instance() == Singleton.instance());
+    }
+}
