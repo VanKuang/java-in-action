a0591486dc2b2ad0b419da95018de305c6b0d934
VanKuang <vanchee@qq.com>
2016-06-18 11:03:36 +0800

    Revise data structure.

---
+++ java-core/src/main/java/cn/van/kuang/hello/world/java/data/structure/recursive/DirectorySizeCalculator.java
@@ -0,0 +1,35 @@
+package cn.van.kuang.hello.world.java.data.structure.recursive;
+
+import java.io.File;
+
+public class DirectorySizeCalculator {
+
+    public long calculate(String path) {
+        return doCalculate(new File(path));
+    }
+
+    public long doCalculate(File file) {
+        if (!file.exists()) {
+            throw new RuntimeException("[" + file.getAbsolutePath() + "] not exist");
+        }
+
+        long size = file.length();
+
+        if (file.isDirectory()) {
+            for (String childFileName : file.list()) {
+                String childFilePath = new File(file, childFileName).getAbsolutePath();
+                size += calculate(childFilePath);
+            }
+        }
+
+        System.out.println("[" + file.getAbsolutePath() + "], size: " + size);
+
+        return size;
+    }
+
+    public static void main(String[] args) {
+        DirectorySizeCalculator calculator = new DirectorySizeCalculator();
+        System.out.println(calculator.calculate("/Users/VanKuang/Development/workspace/java-in-action/java-core"));
+    }
+
+}
---
+++ java-core/src/main/java/cn/van/kuang/hello/world/java/data/structure/recursive/Factorial.java
@@ -0,0 +1,31 @@
+package cn.van.kuang.hello.world.java.data.structure.recursive;
+
+public class Factorial {
+
+    public int factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n can't be negative");
+        }
+
+        if (n == 0) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Factorial factorial = new Factorial();
+
+        try {
+            factorial.factorial(-1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(factorial.factorial(0));
+        System.out.println(factorial.factorial(1));
+        System.out.println(factorial.factorial(3));
+    }
+
+}
