1b3cd7faeaaef77f28671e302fd666a2879261a7
VanKuang <vanchee@qq.com>
2020-03-08 11:19:38 +0800

    Try EHCache with persistent

--- java-core/src/main/java/cn/van/kuang/java/core/cache/EHCache.java
+++ java-core/src/main/java/cn/van/kuang/java/core/cache/EHCache.java
@@ -1,16 +1,23 @@
 package cn.van.kuang.java.core.cache;
 
+import org.ehcache.Cache;
+import org.ehcache.PersistentCacheManager;
 import org.ehcache.UserManagedCache;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
 import org.ehcache.config.builders.UserManagedCacheBuilder;
+import org.ehcache.config.units.EntryUnit;
+import org.ehcache.config.units.MemoryUnit;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
 public class EHCache {
 
     public void cacheByEHCache(int sum) {
-        UserManagedCache<Long, String> userManagedCache = UserManagedCacheBuilder.newUserManagedCacheBuilder(
-                Long.class, String.class).build(true);
+        final UserManagedCache<Long, String> userManagedCache = UserManagedCacheBuilder.newUserManagedCacheBuilder(Long.class, String.class).build(true);
 
         userManagedCache.put(1L, "KB");
         userManagedCache.put(2L, "MJ");
@@ -54,12 +61,37 @@ public class EHCache {
         System.out.println(map.get(1L));
     }
 
+    public void createPersistentCache(final String cacheName) {
+        final PersistentCacheManager persistentCacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+                .with(CacheManagerBuilder.persistence(new File("/Users/VanKuang/tmp", "ehcache")))
+                .withCache(cacheName,
+                        CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                                Long.class,
+                                String.class,
+                                ResourcePoolsBuilder.newResourcePoolsBuilder()
+                                        .heap(10, EntryUnit.ENTRIES)
+                                        .offheap(1, MemoryUnit.MB)
+                                        .disk(20, MemoryUnit.MB, true)
+                        )
+                )
+                .build(true);
+
+        final Cache<Long, String> cache = persistentCacheManager.getCache(cacheName, Long.class, String.class);
+
+        System.out.println(cache.get(1L));
+
+        cache.put(1L, "stillAvailableAfterRestart");
+
+        persistentCacheManager.close();
+    }
+
     public static void main(String[] args) {
         int sum = 1000 * 1000;
 
-        EHCache ehCache = new EHCache();
-        ehCache.cacheByEHCache(sum);
-        ehCache.cacheByHashMap(sum);
+        EHCache EHCache = new EHCache();
+        EHCache.cacheByEHCache(sum);
+        EHCache.cacheByHashMap(sum);
+        EHCache.createPersistentCache("playerCache");
     }
 
 }
